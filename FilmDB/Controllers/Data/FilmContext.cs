04b5ab35c4dbdf3d95bcbab364331c9b5f0fb89d
unknown <marcin.it.zietek@gmail.com>
2025-04-13 14:54:56 +0200

    Added DBConnection

--- FilmDB/Controllers/Data/FilmContext.cs
+++ FilmDB/Controllers/Data/FilmContext.cs
@@ -1,6 +1,14 @@
-﻿namespace FilmDB.Controllers.Data
+﻿using FilmDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmDB.Controllers.Data
 {
-    public class FilmContext
+    public class FilmContext : DbContext
     {
+        public FilmContext(DbContextOptions<FilmContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FilmModel> Films { get; set; }
     }
 }
